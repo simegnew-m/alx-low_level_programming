a7329c8f3b80514c83130f4778f911db68d0af77
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-25 09:48:42 -0800

    # 0x17-doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,27 +1,27 @@
 #include "lists.h"
+
 /**
- * add_dnodeint - adds new node at the beginning of a
- * doubly linked list
- * @head: a variable of type dlistint_t
- * @n: an integer
- * Return:address of new element or NULL on failure
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 
-	if (!new_node)
-	{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	}
-		new_node->n = n;
-		new_node->next = *head;
-		new_node->prev = NULL;
 
-		if (*head != NULL)
-		{
-			(*head)->prev = new_node;
-		}
-		*head = new_node;
-		return (*head);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
 }
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_dlistint - free a dlistint_t list
- * @head: pointer to the beginning of the linked list
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * @index: The node to locate.
  *
- * Return: void
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the address of the located node.
  */
-void free_dlistint(dlistint_t *head)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *next;
-
-	while (head != NULL)
+	for (; index != 0; index--)
 	{
-		next = head->next;
-		free(head);
-		head = next;
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
+
+	return (head);
 }
