87a71deb3c7cf2410e7f80fdc4a4a5b5c6c4f326
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-25 09:37:03 -0800

    # 0x17-doubly linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,15 @@
 
 #include <stddef.h>
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
 typedef struct dlistint_s
 {
 	int n;
