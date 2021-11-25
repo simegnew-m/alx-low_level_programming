c40a4b1373f78aca053c2f3552401ac4125c1bd3
simegnew-m <simegnew.misganaw@gmail.com>
2021-11-25 09:31:40 -0800

    # 0x17-doubly linked lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,16 +1,27 @@
 #include "lists.h"
-
 /**
- * dlistint_len - count the number of nodes in the linked list
- * @h: pointer to the beginning of a linked list
- *
- * Return: number of nodes
+ * add_dnodeint - adds new node at the beginning of a
+ * doubly linked list
+ * @head: a variable of type dlistint_t
+ * @n: an integer
+ * Return:address of new element or NULL on failure
  */
-size_t dlistint_len(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t i;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	for (i = 0; h != NULL; i++)
-		h = h->next;
-	return (i);
+	if (!new_node)
+	{
+		return (NULL);
+	}
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->prev = NULL;
+
+		if (*head != NULL)
+		{
+			(*head)->prev = new_node;
+		}
+		*head = new_node;
+		return (*head);
 }
