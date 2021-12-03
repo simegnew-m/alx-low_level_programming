94404557d530b3b1426e81427fc36978b1acac1f
simegnew-m <simegnew.misganaw@gmail.com>
2021-12-03 00:39:39 -0800

    # 0x18-dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/simegnew-m/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
-export LD_PRELOAD="$PWD/../gigalib.so
+gcc *.o -shared -o libwin.so LD_PRELOAD=$PWD/libwin.so
+./gm 9 8 10 24 75 9
